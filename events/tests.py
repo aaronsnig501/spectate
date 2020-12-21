a5a8cc9fb45df74f0588b4919b339bb25e0d5a85
Aaron Sinnott <aaronsnig@gmail.com>
2020-12-21 15:02:28 -0300

    Add initial tests

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from datetime import datetime
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from django.urls import reverse
+from rest_framework import status
+from .models import Event
+from sports.models import Sport
 
-# Create your tests here.
+
+class EventTestCase(APITestCase):
+    def setUp(self):
+        """Create a new match event"""
+        sport = Sport(name="Football")
+        sport.save()
+
+        event = Event(
+            name="Real Madrid vs Barcelona", start_time=datetime.now(), sport=sport
+        )
+        event.save()
+
+    def test_get_match_by_id(self):
+        """Test get match by ID
+
+        A status of 200 is returned when an ID is provided is to the matches endpoint
+        """
+        url = reverse("matches", kwargs={"pk": 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
