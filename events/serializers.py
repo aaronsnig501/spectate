4b076927c789086ba16366494f9f774f8a8ecc3a
Aaron Sinnott <aaronsnig@gmail.com>
2020-12-26 21:44:19 -0300

    Minor bug fixes with updated documentation

--- events/serializers.py
+++ events/serializers.py
@@ -8,12 +8,28 @@ from selections.models import Selection
 
 
 class EventListSerializer(ModelSerializer):
+    """Event List Serializer
+
+    The amount of data that we need when getting a list of events is less than the
+    amount of information needed for an individual item. For this reason, we have this
+    separate serializer for when we need to view the truncated data
+    """
+
     class Meta:
         fields = ["id", "url", "name", "start_time"]
         model = Event
 
 
 class EventSerializer(ModelSerializer):
+    """Event Serializer
+
+    This serializer will be used to get all information related to an individual event.
+    This will be used for both detail views, and creating new events.
+
+    This serializer also has one additional piece of information, which is the `message`
+    field. This will be used to determine what the client is expecting to accomplish
+    when posting data.
+    """
 
     sport = SportSerializer()
     markets = MarketSerializer()
@@ -24,6 +40,23 @@ class EventSerializer(ModelSerializer):
         model = Event
 
     def create(self, validated_data):
+        """Create Event
+
+        When creating a new event we want to create a new event, with an existing sport,
+        and as the odds will be specific to an event we should create new a new `market`
+        with `selection` objects for each team or player.
+
+        First we'll extract the data from the `validated_data` dict parameter, then get
+        the correlating `Sport` and create a new `Market` with a `Selection` for each item
+        in the `selections` list. This information will then be used create and save
+        the new event
+
+        Args:
+            validated_data (dict): The dict implicitly passed when `.save()` is called
+
+        Returns:
+            Event: The newly created `Event` instance
+        """
         name = validated_data.pop("name")
         start_time = validated_data.pop("start_time")
         validated_sport_data = validated_data.pop("sport")
@@ -31,7 +64,7 @@ class EventSerializer(ModelSerializer):
         validated_selection_data = validated_market_data.pop("selections")
 
         sport = Sport.objects.get(name=validated_sport_data["name"])
-        market = Market.objects.filter(name=validated_market_data["name"])
+        market = Market.objects.create(name=validated_market_data["name"])
 
         selections = [
             Selection(name=selection["name"], odds=selection["odds"], markets=market)
--- events/views.py
+++ events/views.py
@@ -12,7 +12,79 @@ class EventsAPI(APIView):
     serializer_class = EventSerializer
 
     def get(self, request, pk=None, **kwargs):
-        """GET request handler"""
+        """GET request handler
+
+        Handles incoming GET requests for the `matches` endpoint. This handles two
+        potential requests, both the details view and the list view.
+
+        If a `pk` is provided, only the instance that has that ID will be returned,
+        otherwise a list will be returned, filtered and ordered according to the request
+        params.
+
+        Example:
+            Detail View:
+                curl http://127.0.0.1:8000/api/match/6
+
+            Response:
+                {
+                    "id": 6,
+                    "url": "http://127.0.0.1:8000/matches/6",
+                    "name": "Real Madrid vs Barcelona",
+                    "startTime": "2020-12-21T22:03:30Z",
+                    "sport": {
+                        "id": 1,
+                        "name": "Football"
+                    },
+                    "markets": {
+                        "id": 7,
+                        "name": "Winner",
+                        "selections": [
+                            {
+                                "id": 15,
+                                "name": "Real Madrid",
+                                "odds": 10
+                            },
+                            {
+                                "id": 16,
+                                "name": "Barcelona",
+                                "odds": 5.55
+                            }
+                        ]
+                    }
+                }
+
+            List View:
+                curl http://127.0.0.1:8000/api/match/
+
+            Response:
+                [
+                    {
+                        "id": 3,
+                        "url": "http://127.0.0.1:8000/matches/3",
+                        "name": "Real Madrid vs Barcelona",
+                        "startTime": "2020-12-21T22:03:30Z"
+                    },
+                    {
+                        "id": 4,
+                        "url": "http://127.0.0.1:8000/matches/4",
+                        "name": "Real Madrid vs Barcelona",
+                        "startTime": "2020-12-21T22:03:30Z"
+                    },
+                    {
+                        "id": 5,
+                        "url": "http://127.0.0.1:8000/matches/5",
+                        "name": "Real Madrid vs Barcelona",
+                        "startTime": "2020-12-21T22:03:30Z"
+                    },
+                    {
+                        "id": 6,
+                        "url": "http://127.0.0.1:8000/matches/6",
+                        "name": "Real Madrid vs Barcelona",
+                        "startTime": "2020-12-21T22:03:30Z"
+                    }
+                ]
+
+        """
         if pk:
             event = Event.objects.get(id=pk)
             serializer = self.serializer_class(event)
@@ -23,7 +95,24 @@ class EventsAPI(APIView):
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, **kwargs):
-        """POST request handler"""
+        """POST request handler
+
+        This endpoint will be used with two specific goals in mind. The first is to
+        create new events, and the second is to update the odds of a specific event.
+
+        This is accomplished using specific messages. The two types of messages
+        expected are:
+            `NewEvent`: Indicates that a new event is to be created
+            `UpdateOdds`: Indicates that the odds on an events selections are to be
+                          updated
+        
+        Example Usage:
+            curl -X POST http://127.0.0.1:8000/api/match/ \
+                -d '{"message":"NewEvent","name": "Real Madrid vs Barcelona", \
+                "startTime": "2020-12-21T22:03:30Z", "sport": {"id": 1, "name": "Football"}, \
+                "markets": {"id": 1, "name": "Winner", "selections": [{"id": 1, "name": "Real Madrid",\
+                "odds": 10},{"id": 2, "name": "Barcelona", "odds": 5.55}]}}
+        """
         event_serializer = self.serializer_class(data=request.data)
 
         if event_serializer.is_valid():
