825399f8a7c57335d6caca2e4992d543da421f1e
Aaron Sinnott <aaronsnig@gmail.com>
2020-12-27 14:47:32 -0300

    Update URL generation to use proper settings

--- events/models.py
+++ events/models.py
@@ -4,9 +4,6 @@ from sports.models import Sport
 from markets.models import Market
 
 
-BASE_URL = f"http://{settings.ALLOWED_HOSTS[0]}:8000"
-
-
 class Event(models.Model):
     """Event model"""
 
@@ -18,7 +15,7 @@ class Event(models.Model):
 
     @property
     def url(self):
-        return f"{BASE_URL}/api/match/{self.id}"
+        return f"{settings.CURRENT_DOMAIN}/api/match/{self.id}"
 
     def __str__(self):
         return f"{self.name} ({self.sport.name}) - {self.start_time}"
